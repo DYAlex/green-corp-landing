fea029b4027fbd04223adfe2446d67973cd32f00
Yvette Demidova <yvette_demidova@mail.ru>
2022-11-16 18:55:33 +0300

    Изменена шапка сайта. Добавлена функция updateScroll, изменяющая внешний вид шапки в зависимости от события scroll, а также запускающая анимацию счетчика клиентов по достижении его позиции пользователем

--- js/animations.js
+++ js/animations.js
@@ -1,7 +1,5 @@
-// increaseNumberAnimationStep
-// initIncreaseNumberAnimation
-
 const INCREASE_NUMBER_ANIMATION_SPEED = 50;
+let animationInited = false;
 function increaseNumberAnimationStep(i, element, endNumber) {
     if (i <= endNumber) {
         if (i === endNumber) {
@@ -20,7 +18,6 @@ function initIncreaseNumberAnimation() {
     let element = document.querySelector(".features__clients-count");
     increaseNumberAnimationStep(0, element, 5000);
 }
-initIncreaseNumberAnimation();
 
 let budget = document.querySelector("#budget");
 budget.addEventListener("change", function handleSelectChange(event) {
@@ -45,3 +42,20 @@ budget.addEventListener("change", function handleSelectChange(event) {
         document.querySelector("#form").removeChild(otherInput);
     }
 });
+
+function updateScroll() {
+    let header = document.querySelector("header");
+    if (window.scrollY > 0) {
+        header.classList.add("header__scrolled");
+    } else {
+        header.classList.remove("header__scrolled");
+    }
+
+    let windowBottomPosition = window.scrollY + window.innerHeight;
+    let countElementPosition = document.querySelector('.features__clients-count').offsetTop;
+    if (windowBottomPosition >= countElementPosition && !animationInited) {
+        animationInited = true;
+        initIncreaseNumberAnimation();
+    }
+}
+window.addEventListener('scroll', updateScroll);
